fix partial calculation

lib/core/calculate.py
@@ -8,7 +8,7 @@ def reflectance(s11: np.complex, s21: np.complex) -> np.complex:
   """
   calculate reflectance coefficient from S constant
   """
-  X = (s11**2 - s21**2 + 1) / (2 * s11**2)
+  X = (s11**2 - s21**2 + 1) / (2 * s11)
   return X + np.sqrt(X**2 - 1)
 
 def transmitance(s11: np.complex, s21: np.complex) -> np.complex:
@@ -31,7 +31,7 @@ def relative_permeability(reflectance: np.complex, delta_coef: np.complex):
   """
   return (
     (1.0 + reflectance) /
-    delta_coef * (1.0 - reflectance) * np.sqrt((1.0 / lambda0**2) - (1.0 / lambdaC**2))
+    (delta_coef * (1.0 - reflectance) * np.sqrt((1.0 / lambda0**2) - (1.0 / lambdaC**2)))
   )
 
 def relative_permitivity(relative_permeability: np.complex, transmitance: np.complex, d: np.float) -> np.complex:
@@ -39,8 +39,8 @@ def relative_permitivity(relative_permeability: np.complex, transmitance: np.com
   calculate relative permitivity
   """
   return (
-    lambda0**2 / relative_permeability *
-    (1.0 / lambdaC**2) - ((1.0 / (2.0*np.pi*d)) * np.log(1.0 / transmitance))**2
+    (lambda0**2 / relative_permeability) *
+    ((1.0 / lambdaC**2) - ((1.0 / (2.0*np.pi*d)) * np.log(1.0 / transmitance))**2)
   )
 
 def impedance(frequency: np.float, d: np.float, relative_permeability: np.complex, relative_permitivity: np.complex) -> np.complex:
@@ -58,4 +58,4 @@ def absorption(impedance: np.complex) -> np.float:
   """
   calculate absorption
   """
-  return 20.0 * np.log(np.absolute((impedance - 1.0) / (impedance + 1.0)))
+  return 20.0 * np.log10(np.absolute((impedance - 1.0) / (impedance + 1.0)))

lib/data.py
@@ -1,3 +1,4 @@
+import re
 import pandas as pd
 import numpy as np
 
@@ -9,17 +10,25 @@ class InputFile:
     """
     initialization import file from filename
     """
-    self._data = pd.read_csv(filename)
+    # self._data = pd.read_csv(filename)
+    self._data = pd.read_csv(filename, delim_whitespace=True)
 
-    self.frequency = []
+    self._frequency = []
     self._s11 = []
     self._s12 = []
     self._s21 = []
     self._s22 = []
 
     for _, row in self._data.iterrows():
-      self.frequency.append(row["frequency"])
+      # self._frequency.append(np.float("".join(re.findall('(\d+)', row["frequency"]))))
+      self._frequency.append(row["frequency"])
       self._s11.append(complex(row["s11r"], row["s11i"]))
       self._s12.append(complex(row["s12r"], row["s12i"]))
       self._s21.append(complex(row["s21r"], row["s21i"]))
       self._s22.append(complex(row["s22r"], row["s22i"]))
+
+    self.frequency = np.array(self._frequency)
+    self.s11 = np.array(self._s11)
+    self.s12 = np.array(self._s12)
+    self.s21 = np.array(self._s21)
+    self.s22 = np.array(self._s22)

main.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 from lib import InputFile
 from lib.core.calculate import (
   reflectance, 
@@ -10,25 +13,27 @@ from lib.core.calculate import (
 )
 
 d = 4.74e-3
-data = InputFile("./data/data_csv_ujicoba.csv")
+# data = InputFile("./data/data_csv_ujicoba.csv")
+data = InputFile("./data/11-4.74_preprocess.txt")
 
-R = reflectance(data._s11[0], data._s21[0])
-print(R)
+resultRL = []
+for idx, freq in enumerate(data.frequency):
+  s11 = data.s11[idx]
+  s21 = data.s21[idx]
+  
+  R = reflectance(s11, s21)
+  T = transmitance(s11, s21)
+  print(np.absolute(R), np.absolute(T), np.absolute(R) + np.absolute(T))
+  delta = delta_const(d, T)
+  mr = relative_permeability(R, delta)
+  er = relative_permitivity(mr, T, d)
+  Z = impedance(freq, d, mr, er)
+  RL = absorption(Z)
 
-T = transmitance(data._s11[0], data._s21[0])
-print(T)
+  resultRL.append(RL)
+resultRL = np.array(resultRL)
 
-delta = delta_const(d, T)
-print(delta)
+concat = np.vstack((data.frequency, resultRL)).T
 
-mr = relative_permeability(R, delta)
-print(mr)
-
-er = relative_permitivity(mr, T, d)
-print(er)
-
-Z = impedance(1.0e9, d, mr, er)
-print(Z)
-
-RL = absorption(Z)
-print(RL)
+resultDF = pd.DataFrame(data=concat, columns=["frequency", "reflection_loss"])
+resultDF.to_csv("./result/result2.csv", index=None)